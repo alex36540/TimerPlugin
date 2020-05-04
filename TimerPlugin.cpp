abfb5272b7117c00a1075eda243b2cc503d0e47b
alex36540 <alex36540@gmail.com>
2020-05-04 17:28:56 -0400

    Initialized project and added .gitignore file

---
+++ TimerPlugin.cpp
@@ -0,0 +1,12 @@
+#include "TimerPlugin.h"
+
+BAKKESMOD_PLUGIN(TimerPlugin, "Timer Plugin", "1.0", PLUGINTYPE_FREEPLAY)
+
+
+void TimerPlugin::onLoad()
+{
+}
+
+void TimerPlugin::onUnload()
+{
+}
--- TimerPlugin.h
+++ TimerPlugin.h
@@ -1,1 +1,14 @@
 #pragma once
+#pragma comment( lib, "bakkesmod.lib" )
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+
+
+
+class TimerPlugin : public BakkesMod::Plugin::BakkesModPlugin
+{
+private:
+	//private vars
+public:
+	virtual void onLoad();
+	virtual void onUnload();
+};
